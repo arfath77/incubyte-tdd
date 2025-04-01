fad65b6c9165b4b33c0873a95cc7ec1787866b90
arfath77 <arfathtade@gmail.com>
2025-04-02 02:40:15 +0530

    throw an error for negative numbers in input and update tests

--- src/app.js
+++ src/app.js
@@ -6,11 +6,14 @@ const add = (a = '') => {
   if (Array.isArray(delimiter)) a = a.replace(delimiter[0], '');
   a = a.replace(/\n/g, ',');
   const array = a.split(delimiter[1] || ',');
+  const negativeNumbers = array.filter(num => num < 0);
+  if (negativeNumbers.length > 0) {
+    throw new Error(
+      `Negative numbers not allowed: ${negativeNumbers.join(',')}`
+    );
+  }
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
-    if (array[i] < 0) {
-      throw new Error(`number is negative: ${array[i]}`);
-    }
     sum += Number(array[i]);
   }
   return sum;
--- src/app.test.js
+++ src/app.test.js
@@ -7,5 +7,6 @@ test('add', () => {
   expect(add('1,5,10,240')).toBe(256);
   expect(add('1\n5,10,240')).toBe(256);
   expect(add('//;\n1;2')).toBe(3);
-  expect(add('//;\n1;2;-3')).toBe(0);
+  // expect(add('//;\n1;2;-3')).toBe(0);
+  expect(add('//;\n1;2;-3;-5;-6')).toBe(-11);
 });
